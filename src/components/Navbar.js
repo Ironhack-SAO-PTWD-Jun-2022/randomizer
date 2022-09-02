a31b6f817a3a67cd4fd7a5bf7e4b56f68bef4a14
heliodcf <heliodcf@gmail.com>
2022-09-01 20:10:41 -0400

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+    <Link to="/">
+    RANDOMIZER
+    </Link>
+</nav>
+  )
+}
+
+export default Navbar
